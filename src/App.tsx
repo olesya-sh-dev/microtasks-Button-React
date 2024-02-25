31b7ea5a559eb290d50ddd5e09f22905db897079
olesyaShalay <olesyakublickaya@yandex.ru>
2024-02-25 17:10:21 +0300

    universal Component Button

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { MouseEvent } from "react";
+import "./App.css";
+import { Button } from "./components/Button";
 
 function App() {
+  const myFirstSubscriber = () => {
+    console.log("Hello! I am Vasya");
+  };
+
+  const myNewSubscriber = (name: string) => console.log("Hello! I am " + name);
+
+  const foo1 = () => {
+    console.log("100200");
+  };
+
+  const foo2 = (number: number) => console.log(number);
+
+  const Button1 = (name: string, age: number) => {
+    console.log(name, age);
+  };
+  const Button2 = (name: string) => {
+    console.log(name);
+  };
+
+  const Button3 = () => {
+    console.log("I am stupid button");
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button
+        onClick={(event) => {
+          console.log("Hello");
+        }}
+      >
+        BUTTON
+      </button>
+      <button onClick={myFirstSubscriber}>Channel 1</button>
+      <button
+        onClick={(event: MouseEvent<HTMLButtonElement>) =>
+          myNewSubscriber("Ivan")
+        }
+      >
+        Subscriber
+      </button>
+
+      <button onClick={foo1}>Btn1</button>
+      <button onClick={(event: MouseEvent<HTMLButtonElement>) => foo2(100200)}>
+        Btn2
+      </button>
+
+      <Button name="Channel1" callBack={() => Button1("Ivan", 21)} />
+      <Button name="Channel2" callBack={() => Button2("Sasha")} />
+      <Button name="Stupid Button" callBack={Button3} />
     </div>
   );
 }
