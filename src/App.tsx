ac1133a6a44b9b2c09fdbabf1e01db7f4c9cc9bf
olesyaShalay <olesyakublickaya@yandex.ru>
2024-02-25 18:09:36 +0300

    filter

--- src/App.tsx
+++ src/App.tsx
@@ -25,16 +25,32 @@ function App() {
   // const Button3 = () => {
   //   console.log("I am stupid button");
   // };
-  let [a, setA] = useState(1);
-  const onClickHandler = () => {
-    setA(++a);
-    console.log(a);
-  };
+  // let [a, setA] = useState(1);
+  // const onClickHandler = () => {
+  //   setA(++a);
+  //   console.log(a);
+  // };
 
-  const onClickHandler0 = () => {
-    setA((a = 0));
-    console.log(a);
-  };
+  // const onClickHandler0 = () => {
+  //   setA((a = 0));
+  //   console.log(a);
+  // };
+
+  //filter
+  const [money, setMoney] = useState([
+    { banknote: "dollar", nominal: 100, number: "a123456789" },
+    { banknote: "dollar", nominal: 50, number: "b123456789" },
+    { banknote: "ruble", nominal: 100, number: "c123456789" },
+    { banknote: "dollar", nominal: 100, number: "d123456789" },
+    { banknote: "dollar", nominal: 50, number: "e123456789" },
+    { banknote: "ruble", nominal: 100, number: "f123456789" },
+    { banknote: "dollar", nominal: 50, number: "j123456789" },
+    { banknote: "ruble", nominal: 50, number: "h123456789" },
+  ]);
+
+  let currentMoney = money.filter(
+    (filteredMoney) => filteredMoney.banknote === "ruble"
+  );
 
   return (
     <div className="App">
@@ -62,10 +78,22 @@ function App() {
       <Button name="Channel1" callBack={() => Button1("Ivan", 21)} />
       <Button name="Channel2" callBack={() => Button2("Sasha")} />
       <Button name="Stupid Button" callBack={Button3} /> */}
-      <h1>{a}</h1>
+      {/* <h1>{a}</h1>
       <button onClick={onClickHandler}>number</button>
 
-      <button onClick={onClickHandler0}>0</button>
+      <button onClick={onClickHandler0}>0</button> */}
+
+      <ul>
+        {currentMoney.map((objFromMoneyArr, index) => {
+          return (
+            <li key={index}>
+              <span>{objFromMoneyArr.banknote}</span>
+              <span>{objFromMoneyArr.nominal}</span>
+              <span>{objFromMoneyArr.number}</span>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
